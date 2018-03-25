99059340cb63a7bc003ce436f0878234d5f23741
Claudio Mendonca <claudioccm@gmail.com>
2018-03-25 17:55:47 -0400

    index html/css

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,23 +1,5 @@
 $(document).ready(function () {
 
-  var x = $('.smartlink')
-  
-  $.each(x, function(i, val) {
-    var ref = $(this);
-    var ref_top = ref.offset().top;
-    var ref_id = ref.attr('class').slice(-3); // this needs to be more robust
-
-    var content_top = $('.prose__content').offset().top;
-
-    var text_ref = $('.smartlink-content--' + ref_id );
-    text_ref.css({top: ref_top - content_top });
-  });
-
-  $('.smartlink').click(function(){
-    var ref_id = $(this).attr('class').slice(-3);
-    $('.smartlink-content--' + ref_id ).toggleClass('js-active');
-  });
-
   // Smooth Scrolling Function
   $('a[href*=#]:not([href=#])').click(function () {
       var $targ = $(this.hash),
@@ -40,19 +22,9 @@ $(document).ready(function () {
   });
 
 
-  $('.js-submit').click(function() {
-    $('.js-modal').addClass('js-active');
-    $('.js-overlay').addClass('js-active');
-  });
-  
   $('.js-overlay').click(function() {
     $('.js-modal').removeClass('js-active'); // FIX ME!!!
     $(this).removeClass('js-active'); // FIX ME!!!
-    $('.js-result').removeClass('js-active');
   });
-
-  $('.js-comment-trigger').click(function() {
-    $('.js-result').addClass('js-active'); // FIX ME!!!
-  });
-
 }); // doc.ready
+
